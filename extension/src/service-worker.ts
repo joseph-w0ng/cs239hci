124808ad216a9e86a47368307f88645c1a4591b5
josephw0ng <joseph.wong@paramount.com>
2025-05-06 14:40:12 -0700

    fixed build issue

--- extension/src/service-worker.ts
+++ extension/src/service-worker.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import categorizeCookie, { cookieCategories } from './lib/categorize';
-import CookieWithCategory from './app';
 
 let isLoading: boolean = false;
 
